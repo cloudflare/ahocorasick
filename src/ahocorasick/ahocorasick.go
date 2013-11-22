8acc45e0ae617d6da61af51af4370a6608efa07c
John Graham-Cumming <jgc@cloudflare.com>
2013-11-22 09:38:46 -0800

    Initial commit

---
+++ src/ahocorasick/ahocorasick.go
@@ -0,0 +1,232 @@
+// ahocorasick.go: implementation of the Aho-Corasick string matching
+// algorithm. Actually implemented as matching against []byte rather
+// than the Go string type. Throughout this code []byte is referred to
+// as a blice.
+//
+// http://en.wikipedia.org/wiki/Aho%E2%80%93Corasick_string_matching_algorithm
+//
+// Copyright (c) 2013 CloudFlare, Inc.
+
+package ahocorasick
+
+import (
+	"container/list"
+)
+
+// A node in the trie structure used to implement Aho-Corasick
+type node struct {
+	b []byte // The blice at this node
+
+	output bool // True means this node represents a blice that should
+	// be output when matching
+	index int // index into original dictionary if output is true
+
+	counter int // Set to the value of the Matcher.counter when a
+	// match is output to prevent duplicates output
+
+	// The use of fixed size arrays is space-inefficient but fast for
+	// lookups.
+
+	child [256]*node // A non-nil entry in this array means that the
+	// index represents a byte value which can be
+	// appended to the current node. Blices in the
+	// trie are built up byte by byte through these
+	// child node pointers.
+
+	suffix *node // Pointer to the longest possible strict suffix of
+	// this node
+
+	fail *node // Pointer to the next node which is in the dictionary
+	// which can be reached from here following suffixes. Called fail
+	// because it is used to fallback in the trie when a match fails.
+}
+
+// Matcher is returned by NewMatcher and contains a list of blices to
+// match against
+type Matcher struct {
+	root    *node // Points to the root of the trie
+	counter int   // Counts the number of matches done, and is used to
+	// prevent output of multiple matches of the same string
+	trie []node // preallocated block of memory containing all the
+	// nodes
+	extent int // offset into trie that is currently free
+}
+
+// finndBlice looks for a blice in the trie starting from the root and
+// returns a pointer to the node representing the end of the blice. If
+// the blice is not found it returns nil.
+func (m *Matcher) findBlice(b []byte) *node {
+	n := m.root
+
+	for n != nil && len(b) > 0 {
+		n = n.child[int(b[0])]
+		b = b[1:]
+	}
+
+	return n
+}
+
+// getFreeNode: gets a free node structure from the Matcher's trie
+// pool and updates the extent to point to the next free node.
+func (m *Matcher) getFreeNode() *node {
+	m.extent += 1
+
+	return &m.trie[m.extent-1]
+}
+
+// buildTrie builds the fundamental trie structure from a set of
+// blices.
+func (m *Matcher) buildTrie(dictionary [][]byte) {
+
+	// Work out the maximum size for the trie (all dictionary entries
+	// are distinct plus the root). This is used to preallocate memory
+	// for it.
+
+	max := 1
+	for _, blice := range dictionary {
+		max += len(blice)
+	}
+	m.trie = make([]node, max)
+
+	m.root = m.getFreeNode()
+
+	// This loop builds the nodes in the trie by following through
+	// each dictionary entry building the children pointers.
+
+	for i, blice := range dictionary {
+		n := m.root
+		path := make([]byte, 0)
+		for _, b := range blice {
+			path = append(path, b)
+
+			c := n.child[int(b)]
+
+			if c == nil {
+				c = m.getFreeNode()
+				n.child[int(b)] = c
+				c.b = make([]byte, len(path))
+				copy(c.b, path)
+
+				// Nodes directly under the root node will have the
+				// root as their fail point as there are no suffixes
+				// possible.
+
+				if len(path) == 1 {
+					c.fail = m.root
+				}
+
+				c.suffix = m.root
+			}
+
+			n = c
+		}
+
+		// The last value of n points to the node representing a
+		// dictionary entry
+
+		n.output = true
+		n.index = i
+	}
+
+	l := new(list.List)
+	l.PushBack(m.root)
+
+	for l.Len() > 0 {
+		n := l.Remove(l.Front()).(*node)
+
+		for i := 0; i < 256; i++ {
+			c := n.child[i]
+			if c != nil {
+				l.PushBack(c)
+
+				for j := 1; j < len(c.b); j++ {
+					c.fail = m.findBlice(c.b[j:])
+					if c.fail != nil {
+						break
+					}
+				}
+
+				if c.fail == nil {
+					c.fail = m.root
+				}
+
+				for j := 1; j < len(c.b); j++ {
+					s := m.findBlice(c.b[j:])
+					if s != nil && s.output {
+						c.suffix = s
+						break
+					}
+				}
+			}
+		}
+	}
+}
+
+// NewMatcher creates a new Matcher used to match against a set of
+// blices
+func NewMatcher(dictionary [][]byte) *Matcher {
+	m := new(Matcher)
+
+	m.buildTrie(dictionary)
+
+	return m
+}
+
+// NewStringMatcher creates a new Matcher used to match against a set
+// of strings (this is a helper to make initialization easy)
+func NewStringMatcher(dictionary []string) *Matcher {
+	m := new(Matcher)
+
+	d := make([][]byte, 0)
+	for _, s := range dictionary {
+		d = append(d, []byte(s))
+	}
+
+	m.buildTrie(d)
+
+	return m
+}
+
+// Match searches in for blices and returns all the blices found as
+// indexes into the original dictionary
+func (m *Matcher) Match(in []byte) []int {
+	m.counter += 1
+	hits := make([]int, 0)
+
+	n := m.root
+
+	for _, b := range in {
+		c := int(b)
+
+		for n.child[c] == nil && n != m.root {
+			n = n.fail
+		}
+
+		if n.child[c] != nil {
+			f := n.child[c]
+			n = f
+
+			if f.output && f.counter != m.counter {
+				hits = append(hits, f.index)
+				f.counter = m.counter
+			}
+
+			for f.suffix != m.root {
+				f = f.suffix
+				if f.counter != m.counter {
+					hits = append(hits, f.index)
+					f.counter = m.counter
+				} else {
+
+					// There's no point working our way up the
+					// suffixes if it's been done before for this call
+					// to Match. The matches are already in hits.
+
+					break
+				}
+			}
+		}
+	}
+
+	return hits
+}
---
+++ src/ahocorasick/ahocorasick_test.go
@@ -0,0 +1,279 @@
+// ahocorasick_test.go: test suite for ahocorasick
+//
+// Copyright (c) 2013 CloudFlare, Inc.
+
+package ahocorasick
+
+import (
+	"strings"
+	"testing"
+)
+
+func assert(t *testing.T, b bool) {
+	if !b {
+		t.Fail()
+	}
+}
+
+func TestNoPatterns(t *testing.T) {
+	m := NewStringMatcher([]string{})
+	hits := m.Match([]byte("foo bar baz"))
+	assert(t, len(hits) == 0)
+}
+
+func TestNoData(t *testing.T) {
+	m := NewStringMatcher([]string{"foo", "baz", "bar"})
+	hits := m.Match([]byte(""))
+	assert(t, len(hits) == 0)
+}
+
+func TestSuffixes(t *testing.T) {
+	m := NewStringMatcher([]string{"Superman", "uperman", "perman", "erman"})
+	hits := m.Match([]byte("The Man Of Steel: Superman"))
+	assert(t, len(hits) == 4)
+	assert(t, hits[0] == 0)
+	assert(t, hits[1] == 1)
+	assert(t, hits[2] == 2)
+	assert(t, hits[3] == 3)
+}
+
+func TestPrefixes(t *testing.T) {
+	m := NewStringMatcher([]string{"Superman", "Superma", "Superm", "Super"})
+	hits := m.Match([]byte("The Man Of Steel: Superman"))
+	assert(t, len(hits) == 4)
+	assert(t, hits[0] == 3)
+	assert(t, hits[1] == 2)
+	assert(t, hits[2] == 1)
+	assert(t, hits[3] == 0)
+}
+
+func TestInterior(t *testing.T) {
+	m := NewStringMatcher([]string{"Steel", "tee", "e"})
+	hits := m.Match([]byte("The Man Of Steel: Superman"))
+	assert(t, len(hits) == 3)
+	assert(t, hits[2] == 0)
+	assert(t, hits[1] == 1)
+	assert(t, hits[0] == 2)
+}
+
+func TestMatchAtStart(t *testing.T) {
+	m := NewStringMatcher([]string{"The", "Th", "he"})
+	hits := m.Match([]byte("The Man Of Steel: Superman"))
+	assert(t, len(hits) == 3)
+	assert(t, hits[0] == 1)
+	assert(t, hits[1] == 0)
+	assert(t, hits[2] == 2)
+}
+
+func TestMatchAtEnd(t *testing.T) {
+	m := NewStringMatcher([]string{"teel", "eel", "el"})
+	hits := m.Match([]byte("The Man Of Steel"))
+	assert(t, len(hits) == 3)
+	assert(t, hits[0] == 0)
+	assert(t, hits[1] == 1)
+	assert(t, hits[2] == 2)
+}
+
+func TestOverlappingPatterns(t *testing.T) {
+	m := NewStringMatcher([]string{"Man ", "n Of", "Of S"})
+	hits := m.Match([]byte("The Man Of Steel"))
+	assert(t, len(hits) == 3)
+	assert(t, hits[0] == 0)
+	assert(t, hits[1] == 1)
+	assert(t, hits[2] == 2)
+}
+
+func TestMultipleMatches(t *testing.T) {
+	m := NewStringMatcher([]string{"The", "Man", "an"})
+	hits := m.Match([]byte("A Man A Plan A Canal: Panama, which Man Planned The Canal"))
+	assert(t, len(hits) == 3)
+	assert(t, hits[0] == 1)
+	assert(t, hits[1] == 2)
+	assert(t, hits[2] == 0)
+}
+
+func TestSingleCharacterMatches(t *testing.T) {
+	m := NewStringMatcher([]string{"a", "M", "z"})
+	hits := m.Match([]byte("A Man A Plan A Canal: Panama, which Man Planned The Canal"))
+	assert(t, len(hits) == 2)
+	assert(t, hits[0] == 1)
+	assert(t, hits[1] == 0)
+}
+
+func TestNothingMatches(t *testing.T) {
+	m := NewStringMatcher([]string{"baz", "bar", "foo"})
+	hits := m.Match([]byte("A Man A Plan A Canal: Panama, which Man Planned The Canal"))
+	assert(t, len(hits) == 0)
+}
+
+func TestWikipedia(t *testing.T) {
+	m := NewStringMatcher([]string{"a", "ab", "bc", "bca", "c", "caa"})
+	hits := m.Match([]byte("abccab"))
+	assert(t, len(hits) == 4)
+	assert(t, hits[0] == 0)
+	assert(t, hits[1] == 1)
+	assert(t, hits[2] == 2)
+	assert(t, hits[3] == 4)
+
+	hits = m.Match([]byte("bccab"))
+	assert(t, len(hits) == 4)
+	assert(t, hits[0] == 2)
+	assert(t, hits[1] == 4)
+	assert(t, hits[2] == 0)
+	assert(t, hits[3] == 1)
+
+	hits = m.Match([]byte("bccb"))
+	assert(t, len(hits) == 2)
+	assert(t, hits[0] == 2)
+	assert(t, hits[1] == 4)
+}
+
+func TestMatch(t *testing.T) {
+	m := NewStringMatcher([]string{"Mozilla", "Mac", "Macintosh", "Safari", "Sausage"})
+	hits := m.Match([]byte("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36"))
+	assert(t, len(hits) == 4)
+	assert(t, hits[0] == 0)
+	assert(t, hits[1] == 1)
+	assert(t, hits[2] == 2)
+	assert(t, hits[3] == 3)
+
+	hits = m.Match([]byte("Mozilla/5.0 (Mac; Intel Mac OS X 10_7_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36"))
+	assert(t, len(hits) == 3)
+	assert(t, hits[0] == 0)
+	assert(t, hits[1] == 1)
+	assert(t, hits[2] == 3)
+
+	hits = m.Match([]byte("Mozilla/5.0 (Moc; Intel Computer OS X 10_7_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36"))
+	assert(t, len(hits) == 2)
+	assert(t, hits[0] == 0)
+	assert(t, hits[1] == 3)
+
+	hits = m.Match([]byte("Mozilla/5.0 (Moc; Intel Computer OS X 10_7_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.101 Sofari/537.36"))
+	assert(t, len(hits) == 1)
+	assert(t, hits[0] == 0)
+
+	hits = m.Match([]byte("Mazilla/5.0 (Moc; Intel Computer OS X 10_7_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.101 Sofari/537.36"))
+	assert(t, len(hits) == 0)
+}
+
+var bytes = []byte("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36")
+var sbytes = string(bytes)
+var dictionary = []string{"Mozilla", "Mac", "Macintosh", "Safari", "Sausage"}
+var precomputed = NewStringMatcher(dictionary)
+
+func BenchmarkMatchWorks(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		precomputed.Match(bytes)
+	}
+}
+
+func BenchmarkContainsWorks(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		hits := make([]int, 0)
+		for i, s := range dictionary {
+			if strings.Contains(sbytes, s) {
+				hits = append(hits, i)
+			}
+		}
+	}
+}
+
+var dictionary2 = []string{"Googlebot", "bingbot", "msnbot", "Yandex", "Baiduspider"}
+var precomputed2 = NewStringMatcher(dictionary2)
+
+func BenchmarkMatchFails(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		precomputed2.Match(bytes)
+	}
+}
+
+func BenchmarkContainsFails(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		hits := make([]int, 0)
+		for i, s := range dictionary2 {
+			if strings.Contains(sbytes, s) {
+				hits = append(hits, i)
+			}
+		}
+	}
+}
+
+var bytes2 = []byte("Firefox is a web browser, and is Mozilla's flagship software product. It is available in both desktop and mobile versions. Firefox uses the Gecko layout engine to render web pages, which implements current and anticipated web standards. As of April 2013, Firefox has approximately 20% of worldwide usage share of web browsers, making it the third most-used web browser. Firefox began as an experimental branch of the Mozilla codebase by Dave Hyatt, Joe Hewitt and Blake Ross. They believed the commercial requirements of Netscape's sponsorship and developer-driven feature creep compromised the utility of the Mozilla browser. To combat what they saw as the Mozilla Suite's software bloat, they created a stand-alone browser, with which they intended to replace the Mozilla Suite. Firefox was originally named Phoenix but the name was changed so as to avoid trademark conflicts with Phoenix Technologies. The initially-announced replacement, Firebird, provoked objections from the Firebird project community. The current name, Firefox, was chosen on February 9, 2004.")
+var sbytes2 = string(bytes2)
+
+var dictionary3 = []string{"Mozilla", "Mac", "Macintosh", "Safari", "Phoenix"}
+var precomputed3 = NewStringMatcher(dictionary3)
+
+func BenchmarkLongMatchWorks(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		precomputed3.Match(bytes2)
+	}
+}
+
+func BenchmarkLongContainsWorks(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		hits := make([]int, 0)
+		for i, s := range dictionary3 {
+			if strings.Contains(sbytes2, s) {
+				hits = append(hits, i)
+			}
+		}
+	}
+}
+
+var dictionary4 = []string{"12343453", "34353", "234234523", "324234", "33333"}
+var precomputed4 = NewStringMatcher(dictionary4)
+
+func BenchmarkLongMatchFails(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		precomputed4.Match(bytes2)
+	}
+}
+
+func BenchmarkLongContainsFails(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		hits := make([]int, 0)
+		for i, s := range dictionary4 {
+			if strings.Contains(sbytes2, s) {
+				hits = append(hits, i)
+			}
+		}
+	}
+}
+
+var dictionary5 = []string{"12343453", "34353", "234234523", "324234", "33333", "experimental", "branch", "of", "the", "Mozilla", "codebase", "by", "Dave", "Hyatt", "Joe", "Hewitt", "and", "Blake", "Ross", "mother", "frequently", "performed", "in", "concerts", "around", "the", "village", "uses", "the", "Gecko", "layout", "engine"}
+var precomputed5 = NewStringMatcher(dictionary5)
+
+func BenchmarkMatchMany(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		precomputed5.Match(bytes)
+	}
+}
+
+func BenchmarkContainsMany(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		hits := make([]int, 0)
+		for i, s := range dictionary4 {
+			if strings.Contains(sbytes, s) {
+				hits = append(hits, i)
+			}
+		}
+	}
+}
+
+func BenchmarkLongMatchMany(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		precomputed5.Match(bytes2)
+	}
+}
+
+func BenchmarkLongContainsMany(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		hits := make([]int, 0)
+		for i, s := range dictionary4 {
+			if strings.Contains(sbytes2, s) {
+				hits = append(hits, i)
+			}
+		}
+	}
+}
